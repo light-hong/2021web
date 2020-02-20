f39eca54f35181543fa4c6f52ea5441651135b03
Hongliang0513 <1195528273@qq.com>
2020-02-20 22:51:59 +0800

    change

--- src/example/basic-type.ts
+++ src/example/basic-type.ts
@@ -26,3 +26,63 @@ let arr3: Array<string | number>
 arr3 = ['f', 123]
 let arr4: (string | number)[]
 arr4 = [14, 'b']
+
+// 元祖类型(确定长度和类型的数组)
+let tuple: [string, number, boolean]
+tuple = ['sss', 222, true]
+
+// 枚举类型(序列号可以指定，自增)
+enum Roles {
+  SUPER_ADMIN,
+  ADMIN = 6,
+  USER
+}
+console.log(Roles.ADMIN)
+console.log(Roles[7])
+
+// any类型
+let value: any
+value = 123
+value = 'aaa'
+
+// void类型
+const test = (text: string): void => {
+  console.log(text)
+}
+test('abc')
+
+// null和undefined 是所有其他类型的子类型
+let u: undefined
+u = undefined
+// u = 123 报错
+let n: null
+n = null
+// n = 123 报错
+value = undefined
+value = null
+
+// never类型 不可能的类型
+const errorFunc = (message: string): never => {
+  throw new Error(message)
+}
+const infiniteFunc = (): never => {
+  while (true) {}
+}
+
+// object
+let obj = {
+  name: 'Tom'
+}
+function getObj(obj: object): void {
+  console.log(obj)
+}
+getObj(obj)
+
+// 类型断言 类型不确定时用于指定类型 两种方式1. <type>变量;2. 变量 as type
+const getLength = (target: string | number): number => {
+  if ((<string>target).length || (target as string).length === 0) {
+    return (<string>target).length
+  } else {
+    return target.toString().length
+  }
+}
